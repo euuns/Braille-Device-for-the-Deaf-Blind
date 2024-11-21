da522f1c9c1d9968d3ea38ee13dc603d0ded84ab
euuns <108703924+euuns@users.noreply.github.com>
2024-11-22 02:28:23 +0900

    Create ultrasound.py

---
+++ test/sensor/ultrasound.py
@@ -0,0 +1,42 @@
+import RPi.GPIO as gpio
+import time
+
+TRIGER = 24
+ECHO = 23
+
+gpio.setmode(gpio.BCM)
+gpio.setup(TRIGER, gpio.OUT)
+gpio.setup(ECHO, gpio.IN)
+
+startTime = time.time()
+
+try:
+    while True:
+        gpio.output(TRIGER, gpio.LOW)
+        time.sleep(0.1)
+        gpio.output(TRIGER, gpio.HIGH)
+        time.sleep(0.00002)
+        gpio.output(TRIGER, gpio.LOW)
+
+        while gpio.input(ECHO) == gpio.LOW:
+            startTime = time.time()
+        
+        while gpio.input(ECHO) == gpio.HIGH:
+            endTime = time.time()
+        
+        period = endTime - startTime
+        dist1 = round(period * 1000000 / 58, 2)
+        dist2 = round(period * 17241, 2)
+
+        print("Dist1", dist1, "cm", "\t Dist2", dist2, "cm")
+
+        if dist1 < 50 and dist2 < 50:
+            print("detcet")
+        
+        time.sleep(1)
+
+except:
+    gpio.cleanup()
+
+
+//코드 https://hemon.tistory.com/72
